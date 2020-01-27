3faf11d88e28b8369f15489282bb4dd81bdfe3f8
cerebral-caller-0985 <ashaffe@gmail.com>
2020-01-27 23:28:48 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,71 @@
 # My Code here....
+def map_to_negativize(arr)
+  index = 0;
+  arr.length.times do
+    arr[index] = arr[index] * -1
+    index += 1
+  end
+  arr
+end
+
+def map_to_no_change(arr)
+  index = 0;
+  arr.length.times do
+    arr[index] = arr[index]
+    index += 1
+  end
+  arr
+end
+
+def map_to_double(arr)
+  index = 0;
+  arr.length.times do
+    arr[index] = arr[index] * 2
+    index += 1
+  end
+  arr
+end
+
+def map_to_square(arr)
+  index = 0;
+  arr.length.times do
+    arr[index] = arr[index] * arr[index]
+    index += 1
+  end
+  arr
+end
+
+def reduce_to_total(arr, starting_point = 0)
+  total = starting_point
+  index = 0
+  length = arr.length
+  length.times do
+    total += arr[index]
+    index += 1
+  end
+  total
+end
+
+def reduce_to_all_true(arr)
+  index = 0
+  result = true
+  arr.length.times do
+    if(arr[index] == false || arr[index] == nil)
+      result = false
+    end
+    index += 1
+  end
+  result
+end
+
+def reduce_to_any_true(arr)
+  index = 0
+  result = false
+  arr.length.times do
+    if(arr[index])
+      result = true
+    end
+    index += 1
+  end
+  result
+end
